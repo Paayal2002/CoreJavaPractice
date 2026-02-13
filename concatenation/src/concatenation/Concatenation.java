2e0ba7949de974947244c8fc009f373afa06544e
Paayal2002 <payalyamsanwar2002@gmail.com>
2026-02-13 16:18:56 +0530

    Add files via upload

---
+++ concatenation/src/concatenation/Concatenation.java
@@ -0,0 +1,36 @@
+package concatenation;
+
+public class Concatenation {
+	
+	public static void main(String args[])
+	{
+		// Example 1
+        System.out.println("Java " + 1 + 1);
+
+        // Example 2
+        System.out.println("Java " + (1 + 1));
+
+        // Example 3
+        System.out.println(1 + 1 + " Java");
+
+        // Example 4
+        System.out.println(1 + 1 + " Java " + 5 + 3);
+
+        // Example 5
+        System.out.println(1 + 1 + " Java " + (5 + 3));
+
+        // Example 6
+        System.out.println(10 - 5 + " Java " + 2 * 3);
+        
+        // Example 7
+        System.out.println(1 + 1 + " Java " + - 5 + 3);
+        
+        //print and println
+        System.out.print( " Java " );
+        System.out.println( " Java1 " );
+        System.out.println( " Java2 " );
+
+
+	}
+
+}
