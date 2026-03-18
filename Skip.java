da8f43df9d4c2449fc52349bc52108064533b352
Paayal2002 <payalyamsanwar2002@gmail.com>
2026-03-18 16:04:27 +0530

    Add files via upload

---
+++ Skip.java
@@ -0,0 +1,15 @@
+
+public class Skip {
+	//Print characters from ‘A’ to ‘Z’ but skip vowels.
+	public static void main(String args[]) {
+		
+	
+	
+	for( char c='A'; c <='Z' ; c++) {
+		if(c == 'A' || c == 'E' || c == 'I'|| c == 'O' || c == 'U' ) {
+			continue;
+		}
+		System.out.println(c);
+	}
+}
+}
